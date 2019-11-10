5cc6677abc337a45ad4a2318a75ef49d0bdb144b
Angelica  Thompson <i.thompson89@outlook.com>
2019-11-10 21:46:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def line (array)
+  if array.length == 0
+  puts ("The line is currently empty.") 
+elsif
+line = "The line is currently:"
+array.each_with_index do
+  |names,index|
+  line << " #{index+1}. #{names}"
+end   
+puts line 
+end 
+end
+
+def take_a_number(katz_deli, name)
+  if katz_deli.length == 0 
+    katz_deli = katz_deli.push(name)
+   puts  "Welcome, #{name}. You are number #{katz_deli.length} in line."
+ else 
+   katz_deli.each_with_index do 
+     |katz_deli, name|
+     katz_deli << " #{name}"
+end 
+puts katz_deli.push
+end 
+end 
