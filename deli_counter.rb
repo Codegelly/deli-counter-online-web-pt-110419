9fc706909c4c3f9307f8564a57a82c4e1f0921fe
Angelica  Thompson <i.thompson89@outlook.com>
2019-11-10 22:30:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,14 +12,14 @@ end
 end
 
 def take_a_number(katz_deli, name)
-  if katz_deli.length == 0 
+ # if katz_deli.length == 0 
     katz_deli = katz_deli.push(name)
    puts  "Welcome, #{name}. You are number #{katz_deli.length} in line."
- else 
-   katz_deli.each_with_index do 
-     |katz_deli, name|
-     katz_deli << " Welcome, #{name}. You are number #{katz_deli} in line."
-end 
-puts katz_deli 
-end 
+ #else 
+  # katz_deli.each_with_index do 
+   #  |katz_deli, name|
+    # katz_deli << " Welcome, #{name}. You are number #{katz_deli} in line."
+#end 
+#puts katz_deli 
+#end 
 end 
