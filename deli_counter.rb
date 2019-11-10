b276ce0f0bd45ab5ef05a6b1d5573dfd1055a3e8
Angelica  Thompson <i.thompson89@outlook.com>
2019-11-10 21:56:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def take_a_number(katz_deli, name)
  else 
    katz_deli.each_with_index do 
      |katz_deli, name|
-     katz_deli << " Welcome, #{name}. You are number #{katz_deli.push} in line."
+     katz_deli << " Welcome, #{name}. You are number #{katz_deli} in line."
 end 
 puts katz_deli
 end 
